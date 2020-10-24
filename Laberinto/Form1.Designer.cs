bffa61bf8f31a5e7093e5821328c9f6b4ae6636b
Daniel <blasterdaxx2208@gmail.com>
2020-10-24 15:14:06 -0500

    automata con colas y pilas

---
+++ Laberinto/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Laberinto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cambiar Laberinto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(201, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Jugar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(331, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 276);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cola";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(360, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(603, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(564, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Pila";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(567, 41);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(202, 276);
+            this.listBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Laberinto/Form1.cs
@@ -0,0 +1,646 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laberinto
+{
+    public partial class Form1 : Form
+    {
+        int[,] laberinto = new int[10, 10];
+        string a = "";
+        string inicio = "00";
+        string fin = "99";
+        string posicion_actual = "";
+        Queue<string> cola = new Queue<string>();
+        Stack<string> pila = new Stack<string>();
+        List<string> Camino = new List<string>();
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            
+
+            
+            
+            
+        }
+
+        public void reinicio_laberinto()
+        {
+            var rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    var peso = rand.Next(1, 11);
+                    if (peso > 3)
+                    {
+                        laberinto[i, j] = 1;
+                    }
+                    else
+                    {
+                        laberinto[i, j] = 0;
+                    }
+
+                    a += laberinto[i, j] + ",";
+
+                }
+                System.Console.WriteLine(a);
+                a = "";
+            }
+
+            laberinto[0, 0] = laberinto[9, 9] = 1;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            reinicio_laberinto();
+            SolidBrush rojo = new SolidBrush(Color.Red);
+            SolidBrush verde = new SolidBrush(Color.Green);
+
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    if (laberinto[i, j].Equals(1))
+                    {
+                        Rectangle rect= new Rectangle(j*20, i*20, 20, 20);
+                        e.Graphics.FillRectangle(verde, rect);
+                    }
+                    else
+                    {
+                        Rectangle rect=new Rectangle(j*20, i*20, 20, 20);
+                        e.Graphics.FillRectangle(rojo, rect);
+                    }
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+
+        }
+
+        public string Izquierda_cola(string casilla)
+        {
+            int fila = int.Parse(casilla[0].ToString());
+            int columna = int.Parse(casilla[1].ToString());
+            int aux = fila - 1;
+            string paso = aux.ToString() + columna.ToString();
+            try
+            {
+                if (laberinto[aux, columna].Equals(1))
+                {
+                    if (Camino.Contains(paso))
+                    {
+                        return "sin camino";
+
+                    }
+                    else
+                    {
+                        if(fin.Equals(paso))
+                        {
+                            cola.Enqueue(paso);
+                            label2.Text = "Solucion encontrada";
+                            return "Solucion encontrada";
+                        }
+
+                        Camino.Add(paso);
+                        cola.Enqueue(paso);
+                        return "nodo añadido";
+                        
+                    }
+
+
+                }
+                else
+                {
+                    return "sin camino";
+                }
+            }
+            catch
+            {
+                return "sin camino";
+            }
+        }
+
+
+        public string Derecha_cola(string casilla)
+        {
+            int fila = int.Parse(casilla[0].ToString());
+            int columna = int.Parse(casilla[1].ToString());
+            int aux = fila + 1;
+            string paso = aux.ToString() + columna.ToString();
+            try
+            {
+                if (laberinto[aux, columna].Equals(1))
+                {
+                    if (Camino.Contains(paso))
+                    {
+                        return "sin camino";
+
+                    }
+                    else
+                    {
+                        if (fin.Equals(paso))
+                        {
+                            cola.Enqueue(paso);
+                            label2.Text = "Solucion encontrada";
+                            return "Solucion encontrada";
+
+                        }
+
+                        Camino.Add(paso);
+                        cola.Enqueue(paso);
+                        return "nodo añadido";
+
+                    }
+
+
+                }
+                else
+                {
+                    return "sin camino";
+                }
+            }
+            catch
+            {
+                return "sin camino";
+            }
+        }
+
+        public string Abajo_cola(string casilla)
+        {
+            int fila = int.Parse(casilla[0].ToString());
+            int columna = int.Parse(casilla[1].ToString());
+            int aux = columna + 1;
+            string paso = fila.ToString() + aux.ToString();
+            try
+            {
+                if (laberinto[fila, aux].Equals(1))
+                {
+                    if (Camino.Contains(paso))
+                    {
+                        return "sin camino";
+
+                    }
+                    else
+                    {
+                        if (fin.Equals(paso))
+                        {
+                            cola.Enqueue(paso);
+                            label2.Text = "Solucion encontrada";
+                            return "Solucion encontrada";
+                        }
+
+                        Camino.Add(paso);
+                        cola.Enqueue(paso);
+                        return "nodo añadido";
+
+                    }
+
+
+                }
+                else
+                {
+                    return "sin camino";
+                }
+            }
+            catch
+            {
+                return "sin camino";
+            }
+        }
+
+
+        public string Arriba_cola(string casilla)
+        {
+            int fila = int.Parse(casilla[0].ToString());
+            int columna = int.Parse(casilla[1].ToString());
+            int aux = columna - 1;
+            string paso = fila.ToString() + aux.ToString();
+            try
+            {
+                if (laberinto[fila, aux].Equals(1))
+                {
+                    if (Camino.Contains(paso))
+                    {
+                        return "sin camino";
+
+                    }
+                    else
+                    {
+                        if (fin.Equals(paso))
+                        {
+                            cola.Enqueue(paso);
+                            label2.Text = "Solucion encontrada";
+                            return "Solucion encontrada";
+                        }
+
+                        Camino.Add(paso);
+                        cola.Enqueue(paso);
+                        return "nodo añadido";
+
+                    }
+
+
+                }
+                else
+                {
+                    return "sin camino";
+                }
+            }
+            catch
+            {
+                return "sin camino";
+            }
+        }
+
+
+
+        public string Izquierda_pila(string casilla)
+        {
+            int fila = int.Parse(casilla[0].ToString());
+            int columna = int.Parse(casilla[1].ToString());
+            int aux = fila - 1;
+            string paso = aux.ToString() + columna.ToString();
+            try
+            {
+                if (laberinto[aux, columna].Equals(1))
+                {
+                    if (Camino.Contains(paso))
+                    {
+                        return "sin camino";
+
+                    }
+                    else
+                    {
+                        if (fin.Equals(paso))
+                        {
+                            pila.Push(paso);
+                            label3.Text = "Solucion encontrada";
+                            return "Solucion encontrada";
+                        }
+
+                        Camino.Add(paso);
+                        pila.Push(paso);
+                        return "nodo añadido";
+
+                    }
+
+
+                }
+                else
+                {
+                    return "sin camino";
+                }
+            }
+            catch
+            {
+                return "sin camino";
+            }
+        }
+
+
+        public string Derecha_pila(string casilla)
+        {
+            int fila = int.Parse(casilla[0].ToString());
+            int columna = int.Parse(casilla[1].ToString());
+            int aux = fila + 1;
+            string paso = aux.ToString() + columna.ToString();
+            try
+            {
+                if (laberinto[aux, columna].Equals(1))
+                {
+                    if (Camino.Contains(paso))
+                    {
+                        return "sin camino";
+
+                    }
+                    else
+                    {
+                        if (fin.Equals(paso))
+                        {
+                            pila.Push(paso);
+                            label3.Text = "Solucion encontrada";
+                            return "Solucion encontrada";
+
+                        }
+
+                        Camino.Add(paso);
+                        pila.Push(paso);
+                        return "nodo añadido";
+
+                    }
+
+
+                }
+                else
+                {
+                    return "sin camino";
+                }
+            }
+            catch
+            {
+                return "sin camino";
+            }
+        }
+
+        public string Abajo_pila(string casilla)
+        {
+            int fila = int.Parse(casilla[0].ToString());
+            int columna = int.Parse(casilla[1].ToString());
+            int aux = columna + 1;
+            string paso = fila.ToString() + aux.ToString();
+            try
+            {
+                if (laberinto[fila, aux].Equals(1))
+                {
+                    if (Camino.Contains(paso))
+                    {
+                        return "sin camino";
+
+                    }
+                    else
+                    {
+                        if (fin.Equals(paso))
+                        {
+                            pila.Push(paso);
+                            label3.Text = "Solucion encontrada";
+                            return "Solucion encontrada";
+                        }
+                        Camino.Add(paso);
+                        pila.Push(paso);
+                        return "nodo añadido";
+
+                    }
+
+
+                }
+                else
+                {
+                    return "sin camino";
+                }
+            }
+            catch
+            {
+                return "sin camino";
+            }
+        }
+
+
+        public string Arriba_pila(string casilla)
+        {
+            int fila = int.Parse(casilla[0].ToString());
+            int columna = int.Parse(casilla[1].ToString());
+            int aux = columna - 1;
+            string paso = fila.ToString() + aux.ToString();
+            try
+            {
+                if (laberinto[fila, aux].Equals(1))
+                {
+                    if (Camino.Contains(paso))
+                    {
+                        return "sin camino";
+
+                    }
+                    else
+                    {
+                        if (fin.Equals(paso))
+                        {
+                            pila.Push(paso);
+                            label3.Text = "Solucion encontrada";
+                            return "Solucion encontrada";
+                        }
+                        Camino.Add(paso);
+                        pila.Push(paso);
+                        return "nodo añadido";
+
+                    }
+
+
+                }
+                else
+                {
+                    return "sin camino";
+                }
+            }
+            catch
+            {
+                return "sin camino";
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+            listBox1.Items.Clear();
+           
+            Camino.Clear();
+            cola.Clear();
+            
+            cola.Enqueue(inicio);
+            Camino.Add(inicio);
+            
+            inicio = "00";
+            posicion_actual = "";
+
+            Cola();
+
+            listBox2.Items.Clear();
+            Camino.Clear();
+            pila.Clear();
+            Camino.Add(inicio);
+            pila.Push(inicio);
+            inicio = "00";
+            posicion_actual = "";
+
+            Pila();
+
+        }
+
+        public void Cola()
+        {
+            string estado_actual;
+            string ruta;
+            while(cola.Count()>0)
+            {
+                ruta = "";
+                foreach(string coordenada in cola)
+                {
+                    ruta += coordenada+",";
+                }
+
+                listBox1.Items.Add(ruta);
+                
+                estado_actual = "";
+                estado_actual = cola.Dequeue();
+                System.Console.WriteLine(estado_actual);
+                if(Arriba_cola(estado_actual).Equals("Solucion encontrada"))
+                {
+                    ruta = "";
+                    foreach (string coordenada in cola)
+                    {
+                        ruta += coordenada + ",";
+                    }
+
+                    listBox1.Items.Add(ruta);
+                    break;
+
+                }
+
+                if (Abajo_cola(estado_actual).Equals("Solucion encontrada"))
+                {
+                    ruta = "";
+                    foreach (string coordenada in cola)
+                    {
+                        ruta += coordenada + ",";
+                    }
+
+                    listBox1.Items.Add(ruta);
+                    break;
+
+                }
+
+                if (Derecha_cola(estado_actual).Equals("Solucion encontrada"))
+                {
+                    ruta = "";
+                    foreach (string coordenada in cola)
+                    {
+                        ruta += coordenada + ",";
+                    }
+
+                    listBox1.Items.Add(ruta);
+                    break;
+
+                }
+
+                if (Izquierda_cola(estado_actual).Equals("Solucion encontrada"))
+                {
+                    ruta = "";
+                    foreach (string coordenada in cola)
+                    {
+                        ruta += coordenada + ",";
+                    }
+
+                    listBox1.Items.Add(ruta);
+                    break;
+
+                }
+
+                if (cola.Count().Equals(0))
+                {
+
+                    label2.Text = "Solucion no encontrada";
+                }
+
+
+
+
+            }
+
+        }
+
+
+
+        public void Pila()
+        {
+            string estado_actual;
+            string ruta;
+            while (pila.Count() > 0)
+            {
+                ruta = "";
+                foreach (string coordenada in pila)
+                {
+                    ruta += coordenada + ",";
+                }
+
+                listBox2.Items.Add(ruta);
+
+                estado_actual = "";
+                estado_actual = pila.Pop();
+                System.Console.WriteLine(estado_actual);
+                if (Arriba_pila(estado_actual).Equals("Solucion encontrada"))
+                {
+                    ruta = "";
+                    foreach (string coordenada in pila)
+                    {
+                        ruta += coordenada + ",";
+                    }
+
+                    listBox2.Items.Add(ruta);
+                    break;
+
+                }
+
+                if (Abajo_pila(estado_actual).Equals("Solucion encontrada"))
+                {
+                    ruta = "";
+                    foreach (string coordenada in pila)
+                    {
+                        ruta += coordenada + ",";
+                    }
+
+                    listBox2.Items.Add(ruta);
+                    break;
+
+                }
+
+                if (Derecha_pila(estado_actual).Equals("Solucion encontrada"))
+                {
+                    ruta = "";
+                    foreach (string coordenada in pila)
+                    {
+                        ruta += coordenada + ",";
+                    }
+
+                    listBox2.Items.Add(ruta);
+                    break;
+
+                }
+
+                if (Izquierda_pila(estado_actual).Equals("Solucion encontrada"))
+                {
+                    ruta = "";
+                    foreach (string coordenada in pila)
+                    {
+                        ruta += coordenada + ",";
+                    }
+
+                    listBox2.Items.Add(ruta);
+                    break;
+
+                }
+
+                if (pila.Count().Equals(0))
+                {
+
+                    label3.Text = "Solucion no encontrada";
+                }
+
+
+
+
+            }
+
+        }
+    }
+}
